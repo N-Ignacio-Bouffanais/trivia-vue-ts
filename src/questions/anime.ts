5ff5afb310029eb1d897f030d6fd8e6177908c5e
Nicolas <nicolas.bouffanais.1999@gmail.com>
2022-11-30 19:21:37 -0300

    I created some questions and install pinia to manage the state

---
+++ src/questions/anime.ts
@@ -0,0 +1,48 @@
+const anime_questions = [
+  {
+    id: 1,
+    question: "Cual es el nombre del protagonista de one piece?",
+    answer: "Monkey D Luffy",
+  },
+  {
+    id: 2,
+    question:
+      "Cual es el numero que lleva Hizoka durante el examen del cazador?",
+    answer: "El numero 4",
+  },
+  {
+    id: 3,
+    question:
+      "Cual es la esfera del dragon que Goku guardaba en su casa en Dragon Ball?",
+    answer: "La esfera de 4 estrellas",
+  },
+  {
+    id: 4,
+    question: "Cual es el nombre del primer pokemon de ASH Ketchup?",
+    answer: "Pikachu",
+  },
+  {
+    id: 5,
+    question:
+      "En el anime de HXH 2011, a que categoria de Hatsu pertenecia Gon?",
+    answer: "Intensificador",
+  },
+  {
+    id: 6,
+    question:
+      "En el anime de Hajime no Ippo, cual es el nombre del maximo rival de Makunouchi Ippo?",
+    answer: "Takeshi Sendou",
+  },
+  {
+    id: 7,
+    question:
+      "En el anime de Dragon Ball Z, Cual es el nombre del hijo de Vegeta?",
+    answer: "Trunks",
+  },
+  {
+    id: 8,
+    question: "En el anime de Naruto, cual es el apodo de Kakashi Hatake?",
+    answer: "El ninja que copia",
+  },
+];
+export default anime_questions
---
+++ src/questions/history.ts
@@ -0,0 +1,45 @@
+const history_questions = [
+  {
+    id: 1,
+    question: "En que año se descubrio América?",
+    answer: "1492",
+  },
+  {
+    id: 2,
+    question: "De que color era el caballo blanco de napoleon?",
+    answer: "Blanco",
+  },
+  {
+    id: 3,
+    question:
+      "Cuales fueron las ciudades de Japon donde cayeron bombas atomicas?",
+    answer: "Hiroshima y Nagasaki",
+  },
+  {
+    id: 4,
+    question: "Como se llama el primer hombre que piso la Luna?",
+    answer: "Neil Armstrong",
+  },
+  {
+    id: 5,
+    question: "Donde sucedio el terremoto mas grande de la historia?",
+    answer: "Valdivia, Chile",
+  },
+  {
+    id: 6,
+    question: "En que fecha se anuncio el primer Iphone?",
+    answer: "9 de enero de 2007",
+  },
+  {
+    id: 7,
+    question: "En que fecha se cayo el muro de Berlin?",
+    answer: "9 de noviembre de 1989",
+  },
+  {
+    id: 8,
+    question: "Quien fue el inventor de la television?",
+    answer: "John Logie Baird en 1923",
+  },
+];
+
+export default history_questions
