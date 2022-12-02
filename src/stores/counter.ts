f3056ef7c773811816db18585783a42a767093b9
Nicolas <nicolas.bouffanais.1999@gmail.com>
2022-12-01 23:04:01 -0300

    I did it the question title and the displays hide and show correctly

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,15 +1,15 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-export const useCounterStore =  defineStore('counter',{
-    state: () => ({
-        counter: 1,
-    }),
-    actions:{
-        incrementBy( value: number) {
-            this.counter += value;
-        },
-        decrementBy( value: number) {
-            this.counter -= value;
-        },
-    }
-})
+export const useCounterStore = defineStore("counter", {
+  state: () => ({
+    counter: 1,
+  }),
+  actions: {
+    incrementBy(value: number) {
+      this.counter += value;
+    },
+    decrementBy(value: number) {
+      this.counter -= value;
+    },
+  },
+});
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,89 +1,162 @@
 <script lang="ts" setup>
-import { ref, unref, onMounted } from 'vue'
+import { ref, unref } from "vue";
 import { useCounterStore } from "../stores/counter";
 const counterStore = useCounterStore();
 
-let selected = ref(false)
+let selected = ref(false);
+let category_sel = ref(true);
 
 const showquestion = () => {
-  return selected.value = true
-}
+  return (selected.value = true), (category_sel.value = false);
+};
 const hidequestion = () => {
-  return selected.value = false
-}
-// const category = ref(null);
-// onMounted(() => {
-//   const height = category.value.clientHeight;
-// });
-
+  return (selected.value = false), (category_sel.value = true);
+};
 </script>
 <template>
   <div class="game-container">
-    <div v-if="(unref(selected) === true)">
-      {{ counterStore.counter }}
-      <button>►</button>
-      <button>◄</button>
-      <button @click="hidequestion" >Elegir otra categoria</button>
+    <div v-show="unref(selected) === true" class="question container">
+      <div class="list-title">
+        <h1>Pregunta numero: {{ counterStore.counter }}</h1>
+        <button class="btn largue" @click="hidequestion">
+          Elegir otra categoria
+        </button>
+      </div>
+      <div class="arrows">
+        <button class="btn short" @click="
+          () => {
+            if (counterStore.counter > 1) {
+              counterStore.decrementBy(1);
+            }
+          }
+        ">
+          ◄ anterior
+        </button>
+        <button class="btn short" @click="
+          () => {
+            if (counterStore.counter < 8) {
+              counterStore.incrementBy(1);
+            }
+          }
+        ">
+          siguiente ►
+        </button>
+      </div>
     </div>
-    <div class="category" id="category">
-    <button @click="showquestion" class="history">HISTORIA</button>
-    <button @click="showquestion" class="math">MATEMATICAS</button>
-    <button @click="showquestion" class="videogames">VIDEOJUEGOS</button>
-    <button @click="showquestion" class="anime">ANIME</button>
-    <button @click="showquestion" class="music">MUSICA</button>
-  </div>
+    <div v-show="unref(category_sel) === true" class="category container">
+      <button @click="showquestion" class="btn history">HISTORIA</button>
+      <button @click="showquestion" class="btn math">MATEMATICAS</button>
+      <button @click="showquestion" class="btn videogames">VIDEOJUEGOS</button>
+      <button @click="showquestion" class="btn anime">ANIME</button>
+      <button @click="showquestion" class="btn music">MUSICA</button>
+    </div>
   </div>
 </template>
 <style scoped lang="scss">
 .game-container {
   padding: 0 8vw;
-}
-.category {
-  margin: 7rem auto;
-  display: grid;
-  grid-auto-rows: 7rem;
-  grid-template-columns: repeat(auto-fill, minmax(26rem, 1fr));
-  justify-items: center;
-  gap: 1rem;
-  button {
+
+  .container {
+    margin: 7rem auto;
+    justify-items: center;
+    display: grid;
+  }
+
+  .btn {
     border: none;
-    width: 24rem;
-    height: 5.2rem;
-    font-size: 1.6rem;
-    border-radius: 0.7rem;
-    color: white;
-    font-weight: 500;
-    letter-spacing: 0.1rem;
     cursor: pointer;
   }
-  .history {
-    background-color: #e91e63;
-    &:hover{
+
+  .question {
+    h1 {
+      font-size: 3.4rem;
+      color: white;
+      margin-right: 1rem;
+    }
+
+    .list-title {
+      display: flex;
+      align-items: center;
+      width: 80vw;
+      justify-content: space-between;
+
+      .largue {
+        background-color: #ff9800;
+        border-radius: 0.8rem;
+        height: 7rem;
+      }
+    }
+
+    .arrows {
+      display: flex;
+      width: 80vw;
+    }
+
+    .short {
+      margin-top: 1rem;
+      margin-right: 1rem;
+    }
+
+    .btn {
+      color: white;
+      background-color: #1e1e1e;
+      font-size: 2rem;
+    }
+  }
+
+  .category {
+    grid-auto-rows: 7rem;
+    grid-template-columns: repeat(auto-fill, minmax(26rem, 1fr));
+    gap: 1rem;
+
+    button {
+      width: 24rem;
+      height: 5.2rem;
+      font-size: 1.6rem;
+      border-radius: 0.7rem;
+      color: white;
+      font-weight: 500;
+      letter-spacing: 0.1rem;
+    }
+
+    .history {
+      background-color: #e91e63;
+
+      &:hover {
         background-color: #e0366f;
+      }
     }
-  }
-  .math {
-    background-color: #1867c0;
-    &:hover{
+
+    .math {
+      background-color: #1867c0;
+
+      &:hover {
         background-color: #2779d6;
+      }
     }
-  }
-  .videogames {
-    background-color: #009688;
-    &:hover{
+
+    .videogames {
+      background-color: #009688;
+
+      &:hover {
         background-color: #1dac9d;
+      }
     }
-  }
-  .anime {
-    background-color: #00bcd4;
-    &:hover{
+
+    .anime {
+      background-color: #00bcd4;
+
+      &:hover {
         background-color: #2ccbe0;
+      }
     }
-  }
-  .music {
-    background-color:#9c27b0;
-    &:hover{
+
+    .music {
+      background-color: #9c27b0;
+
+      &:hover {
         background-color: #b739ce;
+      }
     }
   }
 }
