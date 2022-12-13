dc7b19e360254e17933ca1a8eb6a470f94da41e3
Nicolas <nicolas.bouffanais.1999@gmail.com>
2022-12-13 19:51:03 -0300

    hotfix alternatives ref('')

--- src/components/Alternatives.vue
+++ src/components/Alternatives.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
-// import { ref } from "vue";
-// let picked = ref("");
+import { ref } from "vue";
+let picked = ref("");
 
 </script>
 <script lang="ts" setup>
@@ -17,23 +17,23 @@ const props = defineProps<{
 <template>
   <div>
     <p>
-      <input type="radio" id="one" value="One" v-model="pick" autocomplete="off"/>
+      <input type="radio" id="one" value="One" v-model="picked"/>
       <label for="one">{{ props.alt_two }}</label>
     </p>
     <p>
-      <input type="radio" id="two" value="Two" v-model="pick" autocomplete="off"/>
+      <input type="radio" id="two" value="Two" v-model="picked"/>
       <label for="two">{{ props.alt_one }}</label>
     </p>
     <p>
-      <input type="radio" id="three" value="Three" v-model="pick" autocomplete="off"/>
+      <input type="radio" id="three" value="Three" v-model="picked"/>
       <label for="three">{{ props.alt_four }}</label>
     </p>
     <p>
-      <input type="radio" id="four" value="four" v-model="pick" autocomplete="off"/>
+      <input type="radio" id="four" value="four" v-model="picked"/>
       <label for="four">{{ props.alt_five }}</label>
     </p>
     <p>
-      <input type="radio" id="five" value="five" v-model="pick" autocomplete="off"/>
+      <input type="radio" id="five" value="five" v-model="picked"/>
       <label for="five">{{ props.alt_thee }}</label>
     </p>
   </div>
