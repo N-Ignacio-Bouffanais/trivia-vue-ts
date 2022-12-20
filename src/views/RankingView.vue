70cf7ab2ec963fdb1ba25e405ed829b937af51a2
Nicolas <nicolas.bouffanais.1999@gmail.com>
2022-12-20 00:51:01 -0300

    hot fix, I didnt use onsnaptshot and the data wasnt realtime

--- src/views/RankingView.vue
+++ src/views/RankingView.vue
@@ -5,7 +5,6 @@ import { useCounterStore } from "../stores/counter";
 import { collection, query, getDocs, onSnapshot, orderBy } from "firebase/firestore";
 import { db } from "../main";
 
-
 const counterStore = useCounterStore();
 const router = useRouter();
 const Banner = defineAsyncComponent(() => import('../components/Banner.vue'))
@@ -24,18 +23,21 @@ onMounted( async ()=>{
 
     const q = query(collection(db, "ranking"), orderBy("puntaje", "desc"))
     const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
+
+    onSnapshot(q,(querySnapshot)=>{
+        querySnapshot.forEach((doc) => {
             const score = doc.data()
             html += `
             <div class="banner">
                 <p>Usuario: ${score.usuario}</p>
                 <p>Categoria: ${score.categoria}</p>
                 <p>Puntaje Obtenido: ${score.puntaje}</p>
-                <p>Tiempo: ${score.minutos}: ${score.segundos}</p>
+                <p>Tiempo: ${score.minutos} : ${score.segundos}seg</p>
             </div>
         `
         });
         scoreContainer.innerHTML = html
+    })
 })
 
 
@@ -43,9 +45,9 @@ onMounted( async ()=>{
 <template>
     <div class="container">
         <h1>Ranking</h1>
-        <Banner class="green" :user="counterStore.user_n" :points="counterStore.points" :min="counterStore.min_r"
+        <Banner v-show="" class="yellow" :user="counterStore.user_n" :points="counterStore.points" :min="counterStore.min_r"
             :sec="counterStore.sec_r" />
-        <div class="" id="score"></div>
+        <div class="green" id="score"></div>
         <div class="another-try">
             <Button @click="() => { Play(); ResetPoints() }" :text="'Otra vez!'" :width="'12'" :bg="'#ea1b64'"
                 style="margin: 2rem;" />
@@ -65,17 +67,16 @@ onMounted( async ()=>{
 
     .banner {
         display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(24rem, 1fr));
+        grid-template-columns: repeat(auto-fill, minmax(23rem, 1fr));
         grid-auto-rows: 2.5rem;
         column-gap: 1rem;
         justify-items: start;
         align-items: center;
-        border-radius: 1rem;
-        padding: 0.5rem 1.8rem 0;
-        margin: 2rem auto;
+        padding: 0.3rem 2vw 0;
+        margin: 1.5rem auto;
 
         p {
-            font-size: 1.8rem;
+            font-size: 1.6rem;
             font-weight: 500;
             text-align: start;
         }
@@ -87,62 +88,18 @@ onMounted( async ()=>{
     }
 }
 
-$red-color: #ff0613;
-$red-bg: #2d1f25;
-$blue-color: cyan;
-$blue-bg: #1f232d;
 $green-color: #00e676;
 $green-bg: #1f2d22;
-$orange-color: orangered;
-$orange-bg: #2d221f;
-$purple-color: rgb(170, 2, 170);
-$purple-bg: #251f2d;
 $yellow-color: yellow;
 $yellow-bg: #2b2d1f;
-$dark-blue-color: rgb(2, 78, 219);
-
-.red {
-    outline: $red-color solid 0.2rem;
-    background-color: $red-bg;
-
-    p {
-        color: $red-color;
-    }
-}
-
-.blue {
-    outline: $blue-color solid 0.2rem;
-    background-color: $blue-bg;
-
-    p {
-        color: $blue-color;
-    }
-}
 
 .green {
     outline: $green-color solid 0.2rem;
+    border-radius: 1rem;
     background-color: $green-bg;
 
     p {
         color: $green-color;
-    }
-}
-
-.orange {
-    outline: $orange-color solid 0.2rem;
-    background-color: $orange-bg;
-
-    p {
-        color: $orange-color;
-    }
-}
-
-.purple {
-    outline: $purple-color solid 0.2rem;
-    background-color: $purple-bg;
-
-    p {
-        color: $purple-color;
     }
 }
 
@@ -152,15 +109,6 @@ $dark-blue-color: rgb(2, 78, 219);
 
     p {
         color: $yellow-color;
-    }
-}
-
-.dark-blue {
-    outline: $dark-blue-color solid 0.2rem;
-    background-color: $blue-bg;
-
-    p {
-        color: $dark-blue-color;
     }
 }
 </style>
